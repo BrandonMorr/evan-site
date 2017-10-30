6b201ea6fab6c8107b1ec91a48951cc100521863
brandonmorr <bmorrissey95@gmail.com>
2017-10-29 22:29:21 -0300

    redesign site

--- js/script.js
+++ js/script.js
@@ -1,51 +1,7 @@
 /* Scripts to do some cool stuff */
 
-// Determine when to switch to fixed navbar
-var navPosition = $(window).innerHeight() - 64;
-
 $(document).ready(function() {
 
-  /**
-   * Component initialization.
-   */
-  $('.collapsible').collapsible();
-  $('.parallax').parallax();
+  // doing stuff
 
-  /**
-   * Smooth scrolling to anchor.
-   */
-  $('a[href*="#"]:not([href="#"])').click(function() {
-
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-
-  /**
-   * Hero setup.
-   */
-  $('.parallax-container').css(
-    { height: $(window).innerHeight() - 63 }
-  );
-
-  /**
-   * Navbar functionality.
-   */
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > navPosition) {
-      $(".navbar-fixed").show();
-    }
-
-    if ($(window).scrollTop() < (navPosition - 1)) {
-      $(".navbar-fixed").hide();
-    }
-  });
 });
