d369ab7bdaa0072ecc3b536cb9dcaffe2236ecce
brandonmorr <bmorrissey95@gmail.com>
2018-01-31 21:57:32 -0400

    Blew the dust off this 'son bitch. Added panels, styles, and a new logo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
-/* Scripts to do some cool stuff */
-
 $(document).ready(function() {
+  $('#about-btn').click(function() {
+    if ($('#about').attr('clicked') == 1) {
+      $('#about').animate({ left: '-100%' }, 500);
+      $('#about').attr('clicked', 0);
+    } else {
+      $('#about').animate({ left: '5%' }, 500);
+      $('#about').attr('clicked', 1);
+    }
+  });
 
-  // doing stuff
+  $('#media-btn').click(function() {
+    if ($('#media').attr('clicked') == 1) {
+      $('#media').animate({ left: '-100%' }, 500);
+      $('#media').attr('clicked', 0);
+    } else {
+      $('#media').animate({ left: '5%' }, 500);
+      $('#media').attr('clicked', 1);
+    }
+  });
 
+  $('#events-btn').click(function() {
+    if ($('#events').attr('clicked') == 1) {
+      $('#events').animate({ left: '-100%' }, 500);
+      $('#events').attr('clicked', 0);
+    } else {
+      $('#events').animate({ left: '5%' }, 500);
+      $('#events').attr('clicked', 1);
+    }
+  });
 });
