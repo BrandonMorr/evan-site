f3c335664785f68e21ea094725e2992849bec690
brandonmorr <bmorrissey95@gmail.com>
2018-02-01 23:30:34 -0400

    Added media, cool scroll bars, and a slew of other changes

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   $('#about-btn').click(function() {
     if ($('#about').attr('clicked') == 1) {
       $('#about').animate({ left: '-100%' }, 500);
+      $('#about').animate({ left: '-100%' }, 500);
       $('#about').attr('clicked', 0);
     } else {
       $('#about').animate({ left: '5%' }, 500);
       $('#about').attr('clicked', 1);
     }
   });
-
   $('#media-btn').click(function() {
     if ($('#media').attr('clicked') == 1) {
       $('#media').animate({ left: '-100%' }, 500);
@@ -18,7 +18,6 @@ $(document).ready(function() {
       $('#media').attr('clicked', 1);
     }
   });
-
   $('#events-btn').click(function() {
     if ($('#events').attr('clicked') == 1) {
       $('#events').animate({ left: '-100%' }, 500);
@@ -26,6 +25,24 @@ $(document).ready(function() {
     } else {
       $('#events').animate({ left: '5%' }, 500);
       $('#events').attr('clicked', 1);
+    }
+  });
+  $('#contact-btn').click(function() {
+    if ($('#contact').attr('clicked') == 1) {
+      $('#contact').animate({ left: '-100%' }, 500);
+      $('#contact').attr('clicked', 0);
+    } else {
+      $('#contact').animate({ left: '5%' }, 500);
+      $('#contact').attr('clicked', 1);
+    }
+  });
+  $('#player-btn').click(function() {
+    if ($('#player').attr('clicked') == 1) {
+      $('#player').animate({ bottom: '-10%' }, 250);
+      $('#player').attr('clicked', 0);
+    } else {
+      $('#player').animate({ bottom: '0' }, 250);
+      $('#player').attr('clicked', 1);
     }
   });
 });
