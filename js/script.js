e9dcfb33777008236dfcb920ed52e1e113bc167c
brandonmorr <bmorrissey95@gmail.com>
2018-02-19 15:02:27 -0400

    Intelligent sliding, better screen size compatibilty, some CSS cleanup

--- js/script.js
+++ js/script.js
@@ -10,25 +10,50 @@ $(document).ready(function() {
       function() {
         // each button has a 'panel' attribute to correspond to a panel
         var panel = $(`${$(this).attr('panel')}`);
+        // before we slide anything, reset the panels
+        if (panel.attr('id') != 'player') {
+          $('.panel').each(
+            function() {
+              $(this).animate({ left: '-100%' }, 500);
 
+              if (panel.attr('id') != $(this).attr('id')) {
+                $(this).attr('clicked', 0);
+              }
+            }
+          );
+        }
         // if player button use alternate animation
         if (panel.attr('id') == 'player') {
-          // do the player animation
-          if ($(panel).attr('clicked') == 1) {
-            $(panel).animate({ bottom: '-10%' }, 250);
-            $(panel).attr('clicked', 0);
+          // adjust height if player is open
+          if (panel.attr('clicked') == 1) {
+            $('.panel').each(
+              function() {
+                if (panel.attr('id') != $(this).attr('id')) {
+                  $(this).animate({ height: '100%' }, 250);
+                }
+              }
+            );
+            panel.animate({ bottom: '-10%' }, 250);
+            panel.attr('clicked', 0);
           } else {
-            $(panel).animate({ bottom: '0' }, 250);
-            $(panel).attr('clicked', 1);
+            // adjust height if player is open
+            $('.panel').each(
+              function() {
+                if (panel.attr('id') != $(this).attr('id')) {
+                  $(this).animate({ height: '90%' }, 250);
+                }
+              }
+            );
+            panel.animate({ bottom: '0' }, 250);
+            panel.attr('clicked', 1);
           }
         } else {
-          // otherwise do the panel animation
-          if ($(panel).attr('clicked') == 1) {
-            $(panel).animate({ left: '-100%' }, 500);
-            $(panel).attr('clicked', 0);
+          if (panel.attr('clicked') == 1) {
+            panel.animate({ left: '-100%' }, 500);
+            panel.attr('clicked', 0);
           } else {
-            $(panel).animate({ left: '5%' }, 500);
-            $(panel).attr('clicked', 1);
+            panel.animate({ left: '5%' }, 500);
+            panel.attr('clicked', 1);
           }
         }
       }
